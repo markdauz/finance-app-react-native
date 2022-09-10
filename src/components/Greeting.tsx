fb35ea6e29e688fa301b6c8925302287ab066188
Mark Christopher Dauz <markchristopherdauz@Marks-MacBook-Pro-2.local>
2022-09-10 15:21:23 +0800

    created greeting component

---
+++ src/components/Greeting.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+type GreetingProp = {
+  children: string;
+};
+
+const Greeting = (props: GreetingProp) => {
+  const { children } = props;
+  return (
+    <View style={styles.wrapper}>
+      <Text style={styles.text}>{children}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  wrapper: { maxWidth: 230 },
+  text: {
+    fontSize: 32,
+    lineHeight: 38.73,
+    fontWeight: "600",
+    marginTop: 78,
+    marginBottom: 24,
+  },
+});
+export default Greeting;
--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -1,23 +1,28 @@
 import React from "react";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { StyleSheet, View, SafeAreaView, ScrollView } from "react-native";
 import BalanceCard from "../components/Card/BalanceCard";
+import Greeting from "../components/Greeting";
 
 const Dashboard = () => {
   return (
     <SafeAreaView style={styles.wrapper}>
-      <BalanceCard
-        name="Halca Alane"
-        amount="$ 320, 299"
-        accountNumber="Deposit 1234567890"
-        cardType="Debit card"
-        walletType="E-wallet Top-up"
-      />
+      <ScrollView style={styles.innerWrapper}>
+        <Greeting>Good Morning, Halca!</Greeting>
+        <BalanceCard
+          name="Halca Alane"
+          amount="$ 320, 299"
+          accountNumber="Deposit 1234567890"
+          cardType="Debit card"
+          walletType="E-wallet Top-up"
+        />
+      </ScrollView>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  wrapper: { flex: 1, alignItems: "center" },
+  wrapper: { flex: 1, alignItems: "flex-start", flexDirection: "column" },
+  innerWrapper: { paddingHorizontal: 20 },
 });
 
 export default Dashboard;
